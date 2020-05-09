f3fac580a62ccaf79f711cbb61da13378254bd79
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-05-09 13:47:06 -0400

    Complete ejs templates

--- app.js
+++ app.js
@@ -5,18 +5,41 @@ const ejs = require("ejs");
 
 const app = express();
 
+// set the view engine to ejs
+app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"))
+
+// setting some starting content for hoem page
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
 
 // get response from main route
 app.get("/", function(req, res){
-  res.send("The server is up and running! and I am able to get response")
+  // render function directly looks into views file;thus you dont need to add to the route while rendering the page!
+  res.render("home", {homeStartingEjs:homeStartingContent,
+
+});
 });
 
+app.get("/about", function(req, res){
+  res.render("about", {aboutMeEjs:aboutContent})
 
+});
 
+app.get("/contact", function(req, res){
+  res.render("contact")
+})
 
+app.get("/post", function(req, res){
+  res.render("post")
+})
 
-
-
+app.get("/compose", function(req, res){
+  res.render("compose")
+})
 
 
 
